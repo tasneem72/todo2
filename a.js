b6bf1f5203ae7011c3c578d3aa4143567bedc4c5
tasneem72 <69868973+tasneem72@users.noreply.github.com>
2020-10-10 22:03:50 +0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,30 @@
+const input =document.querySelector('input');
+const btn =document.querySelector('.addtask > button');
+btn.addEventListener('click',Addlist);
+function Addlist(e){
+    const notcompleted =document.querySelector('.notcompleted');
+    const completed =document.querySelector('.completed');
+    const newli =document.createElement('li');
+    const checkbtn =document.createElement('button');
+    const delbtn =document.createElement('button');
+    checkbtn.innerHTML='<i class="fa fa-check"></i>';
+    delbtn.innerHTML='<i class="fa fa-trash"></i>';
+    if(input.value !==''){
+        newli.textContent= input.value;
+        input.value='';
+        notcompleted.appendChild(newli);
+        newli.appendChild(checkbtn);
+        newli.appendChild(delbtn);
+    }
+    checkbtn.addEventListener('click',function(){
+        const   parent  =this.parentNode;
+        parent.remove();
+        completed.appendChild(parent);
+        checkbtn.style.display ='none';
+    });
+    delbtn.addEventListener('click',function(){
+        const   parent  =this.parentNode;
+        parent.remove();
+        
+});
+}
